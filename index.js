b366a47458e27f548ef86a42db67ae1fe2ca2740
Emmanuel <emmanuelamon92@gmail.com>
2021-04-15 20:52:44 -0400

    Complete assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+let add = (a, b) => {
+    return a + b
+}
+let subtract = (a, b) => {
+    return a - b
+}
+let multiply = (a, b) => {
+    return a * b
+}
+let divide = (a, b) => {
+    return a / b
+}
+
+let increment = n => {
+    return n+=1
+}
+let decrement = n => {
+    return n-=1
+}
+let makeInt = n => {
+    return parseInt(n, 10);
+}
+let preserveDecimal = n => {
+    return parseFloat(n)
+}
